5aba9da6b2d7fd98f1abefe5f56179bb73a76b80
Anwasi Ayebaemi <119889433+Ayewealth@users.noreply.github.com>
2024-03-11 16:26:05 +0100

    Correction

--- js/main.js
+++ js/main.js
@@ -33,7 +33,40 @@ if (navClose) {
 }
 
 // SwiperJs
-const swiper = new Swiper(".swiper", {
+const swiper1 = new Swiper(".chefs", {
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  spaceBetween: 30,
+  slidesPerView: 4,
+
+  // breakpoints: {
+  //   // when window width is >= 320px
+  //   320: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 20,
+  //   },
+  //   // when window width is >= 480px
+  //   480: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 30,
+  //   },
+  //   // when window width is >= 640px
+  //   640: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 40,
+  //   },
+  // },
+});
+
+const swiper2 = new Swiper(".testimonial-list", {
   direction: "horizontal",
   loop: true,
   autoplay: {
